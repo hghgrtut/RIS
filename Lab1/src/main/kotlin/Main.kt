38047fab28755f44b1ce201c467f4eb9e1fd8276
Hg Hg <hghghghg.hh39@gmail.com>
2022-09-15 11:03:23 +0300

    done 2 lab

--- Lab1/src/main/kotlin/Main.kt
+++ Lab1/src/main/kotlin/Main.kt
@@ -1,16 +1,42 @@
-import java.util.regex.Pattern
+import java.io.Serializable
 
-object Main {
+class Country() {
 
-    fun isMyPattern(string: String) = isStringHasRegex(string = string, regex = "abcdefghijklmnopqrstuv5320736")
+    val list: MutableList<PopulatedPoint> = mutableListOf()
+}
 
-    fun isValidUrl(string: String): Boolean =
-        if (isStringHasRegex(string = string, regex = HTTP_S_REGEXP))
-            isStringHasRegex(string = string, regex = "$HTTP_S_REGEXP$URL_END_REGEXP")
-        else isStringHasRegex(string = string, regex = URL_END_REGEXP)
+open class PopulatedPoint(var population: Int) : Serializable {
+    private companion object  { const val serialVersionUID = 1L }
+}
 
-    private fun isStringHasRegex(string: String, regex: String) = string.contains(Pattern.compile(regex).toRegex())
+open class Village(population: Int, val cowsPopulation: Int) : PopulatedPoint(population) {
 
-    private const val HTTP_S_REGEXP = "^https?:[0-9]{0,4}//"
-    private const val URL_END_REGEXP = "[a-z]{2,}.[a-z(?=)#/^:]{1,}"
+    private companion object  { const val serialVersionUID = 1L }
+}
+
+open class CityTypeVillage(val buildings: List<Building>, population: Int) : PopulatedPoint(population) {
+
+    private companion object  { const val serialVersionUID = 1L }
+}
+
+open class City(buildings: List<Building>, population: Int, var cityManager: Name) : CityTypeVillage(buildings, population) {
+
+    private companion object  { const val serialVersionUID = 1L }
+}
+
+open class Capital(buildings: List<Building>, population: Int, cityManager: Name, val president: Name) : City(buildings, population, cityManager) {
+
+    private companion object  { const val serialVersionUID = 1L }
+}
+
+@JvmInline
+value class Name(val name: String)
+
+sealed class Building(val length: Float, val width: Float, val height: Float) {
+    class House(var population: Int, length: Float, width: Float, height: Float) : Building(length, width, height)
+    class TrainingCenter(var sport: String, length: Float, width: Float, height: Float) : Building(length, width, height)
+    class Shop(var products: String, length: Float, width: Float, height: Float) : Building(length, width, height)
+    class Market(val shopsInside: Int, length: Float, width: Float, height: Float) : Building(length, width, height)
+    class Office(var workers: Int, length: Float, width: Float, height: Float) : Building(length, width, height)
+    class Factory(var workers: Int, var production: String,     length: Float, width: Float, height: Float) : Building(length, width, height)
 }
