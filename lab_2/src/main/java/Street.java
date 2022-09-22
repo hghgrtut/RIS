efdfb2dd0e230055d830a1fd326c4c2869b6b6de
Your Name <you@example.com>
2022-09-22 08:24:54 +0300

    lab_2 NEW!!!

--- lab_2/src/main/java/Street.java
+++ lab_2/src/main/java/Street.java
@@ -1,26 +1,24 @@
 import java.io.Serializable;
 import java.util.List;
 
-
+//создаем класс улицы, в котором прописаны дома и квартиры этой улицы
 public class Street implements Serializable {
     // перечисление полей класса
     private String name; // имя улицы
     private List<House> houses; // список домов на улице
 
-
     // конструктор класса, задача которого - присвоение значений полям класса
     public Street(String n, List<House> h) {
         name = n;
         houses = h;
-
     }
+
     //вывод названия улицы и домов, расположенных на этой улице, с количеством квартир
-    public void nnn() {
-        System.out.println("Улица: "+ name);
+    public void Information() {
+        System.out.println("Улица: " + name);
         for (int i = 0; i < houses.size(); i++) {
             House house = houses.get(i);
-            System.out.println("дом: "+house.getName()+", количество подъездов: "+house.getCount());
-
+            System.out.println("дом: " + house.getName() + ", количество подъездов: " + house.getCount());
         }
     }
 }
