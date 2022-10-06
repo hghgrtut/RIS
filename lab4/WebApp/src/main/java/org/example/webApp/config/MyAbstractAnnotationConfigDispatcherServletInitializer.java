d29e14d2777e72426900f03dab82b98e9b5e28b8
livinecvaleria <livinecvaleria@gmail.com>
2022-10-06 10:12:10 +0300

    add lab4

---
+++ lab4/WebApp/src/main/java/org/example/webApp/config/MyAbstractAnnotationConfigDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package org.example.webApp.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyAbstractAnnotationConfigDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
