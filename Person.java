5449496555343f69e4019435d2333fbb3a0b5948
Chitakov <70911831+Chitakov@users.noreply.github.com>
2022-10-12 23:30:19 +0300

    add_valid

--- Person.java
+++ Person.java
@@ -1,14 +1,27 @@
 package com.example.lab5.models;
 
 import java.util.Objects;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 /**
  * The type Book.
  */
 public class Person {
     private Long id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 50 characters")
     private String name;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotEmpty(message = "Age should not be empty")
+    @Min(value = 0, message = "Age should be greater than 0")
+    @Max(value = 150, message = "Age should be less than 150")
     private Integer age;
 
     /**
