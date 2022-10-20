7c21bc1bbf6c4b3a4bf4f889f8a78b584ea7b013
LivinecValeria <113585368+LivinecValeria@users.noreply.github.com>
2022-10-20 09:24:50 +0300

    add lab5

---
+++ lab_5/src/main/java/com/gmail/annastel/dao/PersonDAO.java
@@ -0,0 +1,54 @@
+package com.gmail.annastel.dao;
+
+import org.springframework.stereotype.Component;
+import com.gmail.annastel.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
+
+    public Integer saveName(String name) {
+        Person person = new Person();
+        person.setId(++PEOPLE_COUNT);
+        person.setName(name);
+        people.add(person);
+        return PEOPLE_COUNT;
+    }
+}
