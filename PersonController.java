86124ad3af64d5fe038668ad5d34cab7bb4693ad
Chitakov <70911831+Chitakov@users.noreply.github.com>
2022-10-20 09:31:14 +0300

    Lab6_Chitakov

--- PersonController.java
+++ PersonController.java
@@ -1,28 +1,60 @@
-package com.example.lab5.people;
+package com.gmail.chitakov2606nikita.springbootdemo.controller;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import com.gmail.chitakov2606nikita.springbootdemo.model.Person;
+import com.gmail.chitakov2606nikita.springbootdemo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.List;
 
-/**
- * The type Book controller.
- */
-public class PersonController extends HttpServlet {
-    private static final String URL_INDEX = "person.jsp";
-    private final Logger logger = LogManager.getLogger(PersonController.class);
+@Controller
+public class PersonController {
 
-    @Override
-    public void init() {
-       ServletContext ctx = getServletContext();
-       logger.info(ctx.getServletContextName() + " " + (ctx).getServletRegistrations());
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendRedirect(URL_INDEX);
+    @GetMapping("/people")
+    public String findAll(Model model){
+        List<Person> people = personService.findAll();
+        model.addAttribute("people", people);
+        return "people-list";
+    }
+
+    @GetMapping("/person-create")
+    public String createPersonForm(Person person){
+        return "person-create";
+    }
+
+    @PostMapping("/person-create")
+    public String createPerson(Person person){
+        personService.savePerson(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("person-delete/{id}")
+    public String deletePerson(@PathVariable("id") Long id){
+        personService.deleteById(id);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/person-update/{id}")
+    public String updatePersonForm(@PathVariable("id") Long id, Model model){
+        Person person = personService.findById(id);
+        model.addAttribute("person", person);
+        return "person-update";
+    }
+
+    @PostMapping("/person-update")
+    public String updatePerson(Person person){
+        personService.savePerson(person);
+        return "redirect:/people";
     }
 }
