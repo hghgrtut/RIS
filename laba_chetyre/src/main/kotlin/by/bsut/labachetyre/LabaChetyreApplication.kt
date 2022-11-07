2c6214abeb719cd97f28af99775fa6dc5a8bd44c
Hg Hg <hghghghg.hh39@gmail.com>
2022-11-07 19:26:52 +0300

    fix controller url

---
+++ laba_chetyre/src/main/kotlin/by/bsut/labachetyre/LabaChetyreApplication.kt
@@ -0,0 +1,11 @@
+package by.bsut.laba_chetyre
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabaChetyreApplication
+
+fun main(args: Array<String>) {
+    runApplication<LabaChetyreApplication>(*args)
+}
